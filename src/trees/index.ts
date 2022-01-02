90dbe0488b1650a172a80b1166ce4cc2019b6cb2
Brik <brikr@users.noreply.github.com>
2022-01-01 17:19:29 -0700

    test data

--- src/trees/index.ts
+++ src/trees/index.ts
@@ -1,4 +1,27 @@
 export const builtInTrees = {
-  Terraria: "terraria.json",
-  Test: "test.json",
+  Terraria: "Terraria.json",
+  Test: "Test.json",
 };
+
+export interface BaseItem {
+  url?: string;
+  image?: string;
+}
+
+export interface CraftedItem extends BaseItem {
+  madeAt: string | string[];
+  ingredients: Array<{
+    name: string;
+    count: number;
+  }>;
+}
+
+export interface FoundItem extends BaseItem {
+  source: string;
+}
+
+export interface CraftingTree {
+  name: string;
+  version: string;
+  tree: { [name: string]: CraftedItem | FoundItem };
+}
